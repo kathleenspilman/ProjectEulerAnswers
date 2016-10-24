aa88223f36cd3371573736da302e9765b426d713
kspil <kathleen.spilman@gmail.com>
2016-10-24 09:51:03 -0400

    Update 3_LargestPrimeFactor.java

--- Java/3_LargestPrimeFactor.java
+++ Java/3_LargestPrimeFactor.java
@@ -11,29 +11,31 @@ Source and copyright at: https://projecteuler.net/problem=3
 */
 
 import java.lang.Math;
-public class 3_LargestPrimeFactor{
+
+public class LargestPrimeFactor{
+	
 	public static void main(String []args){
-    	long number = 600851475143L;
-    	long answer;
-    	boolean k = true;
-    	boolean stop = true;
-    	int i = 2;
-        do{
-            if ((number % i) == 0){
-                answer = number / i;
-                for (int j = 2; j <= Math.sqrt(answer); j++){
-                    if ((answer % j) == 0){
-                        k = false;
-                    }
-                }
-                if (k == true){
-                    System.out.println(answer);
-                    stop = false;
-                }
-            }
-        i++;  
-        k = true;
-    	} while (stop == true);
-	 }
+    		long number = 600851475143L;
+    		long answer;
+    		boolean k = true;
+    		boolean stop = true;
+    		int i = 2;
+        	do{
+        		if((number % i) == 0){
+        	        	answer = number / i;
+        	        	for (int j = 2; j <= Math.sqrt(answer); j++){
+        	        		if ((answer % j) == 0){
+        	                		k = false;
+        	            		}
+        	        	}
+        	        	if (k == true){
+        	        		System.out.println(answer);
+        	        		stop = false;
+        	        	}
+        	    	}
+        		i++;  
+        		k = true;
+    		} while (stop == true);
+	}
 }	
 
