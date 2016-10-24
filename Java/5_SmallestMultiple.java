df6f0a4a6ea8b950d30b64c76c3963fe31cb085a
kspil <kathleen.spilman@gmail.com>
2016-10-24 00:40:00 -0400

    Update 5_SmallestMultiple.java

--- Java/5_SmallestMultiple.java
+++ Java/5_SmallestMultiple.java
@@ -1,11 +1,15 @@
 /*
+
 5. Smallest Multiple
  
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+Source and copyright: https://projecteuler.net/problem=5
+
 */
 
-public class SmallestMultiple{
+public class 5_SmallestMultiple{
 	
 	public static void main(String args[]) {
 		
