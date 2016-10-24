2421b606e7d796f462ab5a169225f8559294f9a6
kspil <kathleen.spilman@gmail.com>
2016-10-24 12:36:27 -0400

    Update 7_10001stPrime.java

--- Java/7_10001stPrime.java
+++ Java/7_10001stPrime.java
@@ -9,4 +9,30 @@ What is the 10 001st prime number?
 Source and copyright at: https://projecteuler.net/problem=7
 
 */
-
+public class 7_10001stPrime{
+  
+public class HelloWorld{
+  public static void main (String []args){
+    
+    int count = 1;
+    int number = 20;
+    int answer = 0;
+    boolean prime = true;
+    System.out.println("count: " + count + " number: " + number + " answer: " + answer + " prime: " + prime);
+    for (int i = 3; count <= number; i++){
+      for (int j = 2; j < i; j++){
+        if ((i % j) == 0){
+          prime = false;
+          System.out.println("count: " + count + " number: " + number + " answer: " + answer + " prime: " + prime);
+          break;
+        }
+      }
+      if (prime == true){
+        answer = i;
+        count++;
+        System.out.println("count: " + count + " number: " + number + " answer: " + answer + " prime: " + prime);
+      }
+    }
+    System.out.println(answer);
+  }
+}
